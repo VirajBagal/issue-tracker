a59a725564440e1f02f7833f1c3df1a9cc41fe22
Viraj <virajbagal12@gmail.com>
2024-07-27 12:30:22 +0530

    Added active link changes

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from "react-icons/ai"
+import classNames from 'classnames'
+
 
 const Navbar = () => {
+    const path = usePathname()
     const navbarOptions = [
         { name: 'Dashboard', href: '/' },
         { name: 'Issues', href: '/issues' }
@@ -10,15 +15,22 @@ const Navbar = () => {
     return (
         <nav className="flex space-x-3 border-b px-4 h-12 items-center mb-8">
             <Link href="/"><AiFillBug /></Link>
+            {/* text-zinc-300 hover:text-zinc-800' */}
             <ul className='flex space-x-3'>
                 {
                     navbarOptions.map(option => (
-                        <Link key={option.href} className='text-zinc-300 hover:text-zinc-800 transition-colors' href={option.href}>{option.name}</Link>
+                        <Link key={option.href} className={classNames({
+                            'text-zinc-300': path !== option.href,
+                            'text-zinc-800': path === option.href,
+                            'hover:text-zinc-800 transition-colors': true
+                        }
+
+                        )} href={option.href} > {option.name}</Link>
                     )
                     )
                 }
             </ul>
-        </nav>
+        </nav >
     )
 }
 
