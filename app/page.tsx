ba4175b1da8929e0c625e1d6f1ca1f1c303564aa
Viraj <virajbagal12@gmail.com>
2024-07-28 08:54:14 +0530

    added radix-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@radix-ui/themes'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      <Button>Click me</Button>
     </div>
   )
 }
