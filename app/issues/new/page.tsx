0a35cdcc75f8d55ee34bbbd7ed4fdebaa61476bd
Viraj <virajbagal12@gmail.com>
2024-08-01 09:37:09 +0530

    Added simplemde lazyloading

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { TextField, Button, Callout, Text } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from 'next/dynamic';
+// import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -12,6 +13,8 @@ import { createIssueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
 
 type IssueForm = z.infer<typeof createIssueSchema>
 const newIssuePage = () => {
