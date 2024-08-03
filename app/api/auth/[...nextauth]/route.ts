67abfb08c60ed80648bc7f5c1d265b71fda4cfaf
Viraj <virajbagal12@gmail.com>
2024-08-03 17:52:35 +0530

    added user authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import TwitterProvider from "next-auth/providers/twitter";
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        TwitterProvider({
+            clientId: process.env.TWITTER_CLIENT_ID!,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+            version: "2.0"
+        })
+    ],
+    session: {
+        strategy: 'jwt'
+    },
+    debug: true
+}
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
