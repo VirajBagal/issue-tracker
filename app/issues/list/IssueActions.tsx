38301d55403c1f9e26618e540b3d93047eb61e1c
Viraj <virajbagal12@gmail.com>
2024-08-13 12:30:50 +0530

    Build the filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
+import IssueStatusFilter from '@/app/issues/list/IssueStatusFilter'
 
 const IssueActionsPage = () => {
     return (
-        <div className='mb-4'>
+        <div className='mb-4 flex items-center justify-between'>
+            <IssueStatusFilter />
             <Button>
                 <Link href={"/issues/new"}>Create new issue</Link>
             </Button>
